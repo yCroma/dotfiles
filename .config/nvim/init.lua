03b4cb9fba6dae3c8c46e747711afdb575bb8107
yCroma <kuroyuki0728@gmail.com>
2022-02-06 22:04:02 +0900

    Add filetype to be ignored by indent_blankline

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -337,7 +337,9 @@ require('telescope').setup({
 
 -- indent guide
 -- exclude filetype
-vim.cmd("let g:indent_blankline_filetype_exclude = ['gina-status','lsp-installer','markdown','help','NvimTree']")
+vim.cmd(
+  "let g:indent_blankline_filetype_exclude = ['gina-status','lsp-installer','markdown','help','NvimTree', 'quickrun']"
+)
 -- color for indent
 vim.cmd([[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]])
 vim.cmd([[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]])
