38ea198a29f9025930c324f8a71a16c6f8d7410f
yCroma <kuroyuki0728@gmail.com>
2022-02-15 22:45:55 +0900

    add setup scripts for windows

---
+++ windows/install.ps1
@@ -0,0 +1,12 @@
+# powershell
+iwr -useb get.scoop.sh | iex
+iwr https://deno.land/install.ps1 -useb | iex
+
+# winget
+winget install --id Microsoft.Powershell --source winget
+winget install -e --id Git.Git
+
+# scoop
+scoop install gcc
+scoop install vim
+scoop install neovim
---
+++ windows/symbolic_link.bat
@@ -0,0 +1,3 @@
+mklink "%USERPROFILE%\_vimrc" "%USERPROFILE%\dotfiles\.vimrc"
+mklink /d "%USERPROFILE%\vimfiles" "%USERPROFILE%\dotfiles\.vim"
+mklink /d "%LOCALAPPDATA%\nvim" "%USERPROFILE%\dotfiles\.config\nvim"
