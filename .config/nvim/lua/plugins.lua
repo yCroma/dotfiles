480ac572d6ccbd3d7a36fa91d5efe5409f4ee71e
yCroma <kuroyuki0728@gmail.com>
2022-02-21 17:58:26 +0900

    set quit_on_open to false

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -183,7 +183,7 @@ return require('packer').startup(function()
         },
         actions = {
           open_file = {
-            quit_on_open = true,
+            quit_on_open = false,
           },
         },
       })
