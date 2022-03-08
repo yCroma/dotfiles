15909c6afa7a985e9eaa3c42ed198671ed7af2dd
yCroma <kuroyuki0728@gmail.com>
2022-03-08 16:16:41 +0900

    add keymap to ignore <C-z>

--- .config/nvim/lua/maps.lua
+++ .config/nvim/lua/maps.lua
@@ -1,6 +1,8 @@
 -- default
 local opts = { noremap = true, silent = true }
 
+vim.api.nvim_set_keymap('n', '<C-z>', '<Nop>', opts)
+
 vim.api.nvim_set_keymap('n', 'j', 'gjzz', opts)
 vim.api.nvim_set_keymap('n', 'k', 'gkzz', opts)
 vim.api.nvim_set_keymap('n', '{', '{zz', opts)
