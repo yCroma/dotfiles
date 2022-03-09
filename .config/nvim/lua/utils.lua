5bda3d650622716fb47c208d1aa1630243d55777
yCroma <kuroyuki0728@gmail.com>
2022-03-09 14:31:02 +0900

    update utils.lua

--- .config/nvim/lua/utils.lua
+++ .config/nvim/lua/utils.lua
@@ -1,11 +1,12 @@
-local view = require('nvim-tree.view')
 local M = {}
+local tree = require('nvim-tree')
+local view = require('nvim-tree.view')
 
 function M.toggle_tree()
   if view.is_visible() then
     view.close()
   else
-    require('nvim-tree').toggle()
+    tree.toggle()
   end
 end
 
@@ -13,7 +14,7 @@ function M.toggle_vinegar()
   if view.is_visible() then
     view.close()
   else
-    require('nvim-tree').open_replacing_current_buffer()
+    tree.open_replacing_current_buffer()
   end
 end
 
@@ -21,9 +22,8 @@ function M.toggle_vinegar_botright()
   if view.is_visible() then
     view.close()
   else
-    vim.g['nvim-tree#opened#vinegar'] = 1
     vim.cmd([[botright vsplit]])
-    require('nvim-tree').open_replacing_current_buffer()
+    tree.open_replacing_current_buffer()
   end
 end
 
@@ -32,7 +32,7 @@ function M.toggle_vinegar_split()
     view.close()
   else
     vim.cmd([[split]])
-    require('nvim-tree').open_replacing_current_buffer()
+    tree.open_replacing_current_buffer()
   end
 end
 
@@ -41,7 +41,7 @@ function M.toggle_vinegar_topleft()
     view.close()
   else
     vim.cmd([[topleft split]])
-    require('nvim-tree').open_replacing_current_buffer()
+    tree.open_replacing_current_buffer()
   end
 end
 
