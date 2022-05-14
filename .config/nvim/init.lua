702b71b75fe074cc984126191d967108f9313388
yCroma <58209438+yCroma@users.noreply.github.com>
2022-05-14 23:03:10 +0900

    update file types to exclude in indent blank lines

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -422,9 +422,20 @@ require('telescope').setup({
 
 -- indent guide
 -- exclude filetype
-vim.cmd(
-  "let g:indent_blankline_filetype_exclude = ['gina-status','git','lsp-installer','markdown','help','NvimTree', 'quickrun', 'packer', 'lir', 'startify']"
-)
+vim.g['indent_blankline_filetype_exclude'] = {
+  'gina-status',
+  'git',
+  'lsp-installer',
+  'markdown',
+  'help',
+  'NvimTree',
+  'quickrun',
+  'packer',
+  'lir',
+  'startify',
+  'dart',
+  'flutterToolsOutline',
+}
 -- color for indent
 vim.cmd([[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]])
 vim.cmd([[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]])
