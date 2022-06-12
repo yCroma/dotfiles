28aa2095f1e456931d5348717d30fa4569b78885
yCroma <58209438+yCroma@users.noreply.github.com>
2022-06-12 10:09:13 +0900

    remove markdown Naive Linter settings from null-ls

--- .config/nvim/lua/plugins/null-ls.lua
+++ .config/nvim/lua/plugins/null-ls.lua
@@ -4,7 +4,6 @@ local null_ls = require('null-ls')
 local sources = {
   null_ls.builtins.formatting.prettier,
   null_ls.builtins.formatting.stylua,
-  null_ls.builtins.diagnostics.write_good,
   null_ls.builtins.code_actions.gitsigns,
 }
 
